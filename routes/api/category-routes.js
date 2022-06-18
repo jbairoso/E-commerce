9c5afd81fccd92ae4f1d82e6b5513c0c1737c451
jbairoso <jbairoso@gmail.com>
2022-06-17 21:27:41 -0700

    category update delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -56,10 +56,42 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,{
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((dbCategoryData)=>{
+    if (!dbCategoryData){
+      res.status(404).json({message: "No category found with this ID"});
+      return;
+    }
+    res.json(db.dbCategoryData);
+  })
+  .catch((err)=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where:{
+      id: req.params.id,
+    },
+  })
+  .then((dbCategoryData)=>{
+    if (!dbCategoryData){
+      res.status(404).json({message: "No category found with this ID"});
+      return;
+    }
+    res.json(db.dbCategoryData);
+  })
+  .catch((err)=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
