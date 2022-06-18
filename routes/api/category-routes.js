5f6309dd2544af0c56ef320faf8d1af1c9916c5b
jbairoso <jbairoso@gmail.com>
2022-06-17 21:09:07 -0700

    category get post

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { includes } = require("lodash");
+const { resolve } = require("path");
 const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
@@ -23,10 +24,34 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where:{
+      id: req.params.id,
+    },
+  })
+  .then((dbCategoryData)=>{
+    if (!dbCategoryData){
+      res.status(404).json({message: "No category found with this ID"});
+      return;
+    }
+    res.json(db.dbCategoryData);
+  })
+  .catch((err)=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post("/", (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put("/:id", (req, res) => {
