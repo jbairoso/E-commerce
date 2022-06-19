cdb6aa09d87a2988999b4694b53449068e8e359f
jbairoso <jbairoso@gmail.com>
2022-06-18 23:58:16 -0700

    cat error fix

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,7 @@ const { Category, Product, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll()({
+  Category.findAll({
     include: {
       model: Product,
       attributes: ["product_name"],
