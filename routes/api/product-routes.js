7eab13b99a19729cac9bcc4e76bfce951aa73714
jbairoso <jbairoso@gmail.com>
2022-06-18 19:24:22 -0700

    remove ghost code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { resolve } = require("path");
 const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
